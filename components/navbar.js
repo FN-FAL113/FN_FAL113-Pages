62adbec1b3389b976805c437dd7953a412760c55
FN-FAL113 <orbetajeff@gmail.com>
2022-03-06 20:16:39 +0800

    Add files via upload

--- components/navbar.js
+++ components/navbar.js
@@ -8,37 +8,37 @@ class NavBar extends HTMLElement {
 	connectedCallback() {
 		this.innerHTML = `
 		<ul class="bar">
-		<li>
+		<li class="home">
 			<a class="social_link" href="index.html">
            		<img src="https://tinyurl.com/45as4pwk" alt="failed to load image">
-           		<strong style="position: relative; top: 4px;">Home</strong>
+           		<strong style="position: relative; left: 0px; top: 12px">Home</strong>
         	</a>
         </li>
 
         <li>
         	<a class="social_link" href="https://github.com/FN-FAL113/" target=_blank>
            		<img src="https://tinyurl.com/mr36j2pw" alt="failed to load image">
-           		<strong style="position: relative; top: 4px;">Github</strong>
+           		<strong style="position: relative; left:-4px; top: 12px;">Github</strong>
         	</a>
         </li>
 
-        <li>
+        <li class="youtube">
         	<a class="social_link" href="https://www.youtube.com/channel/UCP7YiykhJSFQt1ce7dp-rZg" target=_blank>
-            	<img id="yt_image" src="https://tinyurl.com/4ft8e2v4" alt="failed to load image">
-            	<strong style="position: relative; top: 4px;">Youtube</strong>
+            	<img  src="https://tinyurl.com/4ft8e2v4" alt="failed to load image">
+            	<strong style="position: relative; left:-5px; top: 12px;">Youtube</strong>
         	</a>
         </li>
 
-        <li>
+        <li class="steam">
         	<a class="social_link" href="https://steamcommunity.com/id/JPachelbel" target=_blank>
            		<img src="https://tinyurl.com/2p872evp" alt="failed to load image">
-           		<strong style="position: relative; top: 4px;">Steam</strong>
+           		<strong style="position: relative; left:-5px; top: 12px;">Steam</strong>
         	</a>
         </li>
-        <li>
+        <li class="proj">
         	<a class="social_link" href="projects.html">
            		<img src="https://tinyurl.com/3eeh63af" alt="failed to load image">
-            	<strong style="position: relative; top: 4px;">Projects</strong>
+            	<strong style="position: relative; top: 12px;">Projects</strong>
         	</a>
         </li>
         </ul>
